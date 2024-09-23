cc4107c0c0e96662657de9d2b338c84253b4f230
Santo Shakil <shakil.mezbah@gmail.com>
2024-09-23 15:12:54 +0600

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    capture_log::get_chrome_history(
-        "C:\\Users\\AGL\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History",
-    );
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        println!("Usage: {} <path>", args[0]);
+        std::process::exit(1);
+    }
+
+    capture_log::get_chrome_history(args[1].as_str());
 
     capture_log::get_running_processes();
 
